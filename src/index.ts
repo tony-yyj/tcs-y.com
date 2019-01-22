00b0992b4b9a8e8881c23cdf0b1d68933c377f4c
tony yang <tcsyyj@gmail.com>
2019-01-22 20:31:15 +0800

    init webpack

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function printMe(str: string): void {
+    console.log('print me ', str);
+}
+
+printMe('test333');
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            filename: 'index.html',
+            template: 'index.html',
+            inject: 'head',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
