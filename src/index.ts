ca787617f611a5402fcaa6f364e028ceed630cd3
tony yang <tcsyyj@gmail.com>
2019-04-07 16:48:23 +0800

    将首页风格改为钢铁侠风格

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import './css/index.less';
 
-declare var Particles: any;
-
-window.onload = function () {
-    Particles.init({
-        selector: '.background',
-        color: ['#DA0463', '#404B69', '#DBEDF3'],
-        connectParticles: true
-    });
-};
+// declare var Particles: any;
+//
+// window.onload = function () {
+//     Particles.init({
+//         selector: '.background',
+//         color: ['#DA0463', '#404B69', '#DBEDF3'],
+//         connectParticles: true
+//     });
+// };
